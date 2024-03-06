0da160bb1af4c14e05ee89cc4de1805d5e5beb34
cesarzeballos <cesarezeballos@gmail.com>
2024-03-06 11:55:42 -0300

    Homework n2

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,110 +1,78 @@
-console.log(tempData);
-
 function crearCard(movie) {
 
     const card = document.createElement('div');
     const titleMovie = document.createElement('a');
     const posterMovie = document.createElement('img');
     const descriptionCard = document.createElement('div');
-    const yearMovie = document.createElement('p');
-    const directorMovie = document.createElement('p');
-    const durationMovie = document.createElement('p');
-    const genreMovie = document.createElement('p');
-    const rateMovie = document.createElement('p');
 
     card.classList.add('cardMovie');
     titleMovie.classList.add('titlemovie');
     descriptionCard.classList.add('descriptionBox');
     posterMovie.classList.add('poster');
-    yearMovie.classList.add('descriptionMovie');
 
     titleMovie.textContent = movie.title;
     posterMovie.src = movie.poster;
-    yearMovie.textContent = movie.year;
-    directorMovie.textContent = movie.director;
-    durationMovie.textContent = movie.duration;
-    genreMovie.textContent = movie.genre; // ojo aca que deberia ser distinto creo...
-    rateMovie.textContent = movie.rate;
-    
+    descriptionCard.innerHTML = `
+    <p class="body2">${movie.rate}</p>
+    <p class="body2">${movie.year}</p>
+    <p class="body2">Director: ${movie.director}</p>
+    <p class="body2">Duracion: ${movie.year}</p>
+    <p class="body3">${movie.genre.join(', ')}</p>`
+
     card.appendChild(titleMovie);
     card.appendChild(posterMovie);
     card.appendChild(descriptionCard);
-    descriptionCard.appendChild(yearMovie);
-    descriptionCard.appendChild(directorMovie);
-    descriptionCard.appendChild(durationMovie);
-    descriptionCard.appendChild(genreMovie);
-    descriptionCard.appendChild(rateMovie);
 
     return card;
 }
 
-const container = document.getElementById('moviesRecomendas');
+class Movie {
+    constructor(title, year, director, duration, genre, rate, poster) {
+        this.title = title;
+        this.year = year;
+        this.director = director;
+        this.duration = duration;
+        this.genre = genre;
+        this.rate = rate;
+        this.poster = poster;
+    }
+}
 
-const movies = tempData.map(movie => crearCard(movie));
-
-movies.forEach(card => container.appendChild(card));
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function crearCard(tempData) {
-
-//     const {title, year, director, duration, genre, rate, poster} = tempData;
-
-//     const card = document.createElement('div');
-//     const titleCard = document.createElement('a');
-//     const directorCard = document.createElement('p');
-//     const yearCard = document.createElement('p');
-//     const durationCard = document.createElement('p');
-//     const genreCard = document.createElement('p');
-//     const rateCard = document.createElement('p');
-//     const posterCard = document.createElement('img');
-
-//     card.classList.add('cardMovie');
-//     titleCard.classList.add('titlemovie');
-//     directorCard.classList.add('descriptionMovie');
-//     yearCard.classList.add('descriptionMovie');
-//     durationCard.classList.add('descriptionMovie');
-//     genreCard.classList.add('descriptionMovie');
-//     rateCard.classList.add('descriptionMovie');
-//     posterCard.classList.add('poster');
-
-//     card.appendChild(titleCard);
-//     card.appendChild(posterCard);
-//     card.appendChild(directorCard);
-//     card.appendChild(yearCard);
-//     card.appendChild(durationCard);
-//     card.appendChild(genreCard);
-//     card.appendChild(rateCard);
-
-//     return card;
-// }
-
-// function convertirMovie() {
-
-//     const container = document.getElementById('moviesRecomendas');
-
-//     const mapeo = tempData.map(movie => crearCard(movie));
-
-//     mapeo.forEach(movies => {
-//         container.appendChild(movies)
-//     });
-// }
-
-// function mostrarMovies() {
-
-//     const titleTemp = tempData
-// }
+class Repository {
+    constructor() {
+        this.movies = [];
+    }
 
 
-//usar etiqueta <a hrf="..."> titulo </a>
+    
+    crearMovie({title, year, director, duration, genre, rate, poster}) {
+        const newMovie = new Movie(title, year, director, duration, genre, rate, poster)
+        this.movies.push(newMovie);
+    }
+}
+
+const repository = new Repository;
+
+const refresh = () => {
+    const moviesConteiner = document.getElementById('moviesRecomendas');
+    moviesConteiner.innerHTML = "";
+
+    const movies = repository.movies;
+
+    const htmlMovies = movies.map(movie => crearCard(movie));
+
+    htmlMovies.forEach(card => moviesConteiner.appendChild(card)); 
+}
+
+const addMovie = () => {
+    $.get(`https://students-api.2.us-1.fl0.io/movies`, (data, status) => {
+        data.forEach(movie => {
+            repository.crearMovie(movie);
+            console.log(movie);
+        })
+        refresh();
+
+    })
+}
+
+addMovie();
