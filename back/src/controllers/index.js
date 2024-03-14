0b9325af80076374f867f6ffff57bb10318644e8
cesarzeballos <cesarezeballos@gmail.com>
2024-03-14 10:22:25 -0300

    Homeworks n8

--- back/src/controllers/index.js
+++ back/src/controllers/index.js
@@ -1,8 +1,17 @@
 // van a estar las funciones que van a establecer la LOGICA DEL NEGOCIO de cada una de las rutas. 
 // OJO!!! no es quien lleva a cabo la accion.
+const moviesServices = require("../services/index");
 
-const allMovies = (req, res) => {
-    res.status(200).send("próximamente estarán disponibles los datos de películas.");
+const allMovies = async (req, res) => {
+    try {
+        const movies = await moviesServices.getMovies();
+        res.status(200).json(movies);
+    } catch (error) {
+        res.status(500).json({
+            error: "Error interno del servidor",
+        });
+    };
+    // res.status(200).send("próximamente estarán disponibles los datos de películas.");// esto estaba antes de todo lo anterior.
 };
 
 module.exports = {
--- back/src/server.js
+++ back/src/server.js
@@ -1,7 +1,18 @@
 const express = require("express");
 const router = require("./routers");
+const morgan = require("morgan");
+const cors = require("cors");
 
 const app = express();
+
+app.use(morgan("dev"));
+app.use(cors());
+app.use(express.json());
+
+app.use((req, res, next) => {
+    console.log("Estamos Testeando next.");
+    next();
+});
 
 app.use(router);
 
